9d0876785a9401c59e6d61cfc8d401c3e30e30e4
Mike <mcc32789@gmail.com>
2015-01-25 19:15:03 -0500

    Finish login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    	# if user is authenticated do the following
+    	log_in user
+    	# Log the user in and redirect to the user's show page.
+    	params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    	#defers the real work to the Sessions helper, where we define a remember method that calls user.remember, thereby generating a remember token and saving its digest to the database. It then uses cookies to create permanent cookies for the user id and remember token
+    	redirect_to user
+    	#Rails automatically converts this to the route for the user’s profile page
+      
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
